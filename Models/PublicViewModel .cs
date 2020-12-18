46d9c0113ff55b25844287ccb8a455f00cde8d9f
Zaki <Zaki.jst@gmail.com>
2020-12-18 14:20:14 +0800

    enable public api

---
+++ Models/PublicViewModel .cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace RegineDesignAdmin.Models
+{
+    public class PublicViewModel
+    {
+        public Dictionary<string, List<File>> AllFiles { get; set; }
+        public List<string> Folders { get; set; }
+
+    }
+}
---
+++ ViewApis/PublicApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegineDesignAdmin.LogicLayer;
+using RegineDesignAdmin.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegineDesignAdmin.ViewApis
+{
+    [SkipStatusCodePages]
+    [Route("[controller]/[action]")]
+    public class PublicApiController : Controller
+    {
+        private GoogleStorageLL _googleStorageLL;
+
+        public PublicApiController(GoogleStorageLL googleStorageLL)
+        {
+            _googleStorageLL = googleStorageLL;
+        }
+
+        public async Task<IActionResult> GetAllItemInStorage() {
+            PublicViewModel vm = new PublicViewModel();
+            vm.Folders = new List<string>();
+            vm.AllFiles = new Dictionary<string, List<Models.File>>();
+            var allFolders = await _googleStorageLL.GetAllFoldersAsync();
+            foreach (var oneFolder in allFolders) { 
+                string path = oneFolder.TrimEnd('/');
+                if (!path.Contains("Secret"))
+                {
+                    var allItemInFolder = await _googleStorageLL.GetAllObjectInFolderAsync(path);
+                    vm.Folders.Add(path);
+                    vm.AllFiles.Add(path, allItemInFolder.Files);
+                }
+            }
+            return Ok(vm);
+        }
+
+    }
+}
